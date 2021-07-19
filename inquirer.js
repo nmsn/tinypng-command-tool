958b3742e2cfca56d065ecc088617ed8d85a10dd
nmsn <136696700@qq.com>
2021-07-19 17:23:37 +0800

    拆分函数，增加gui参数输入

--- inquirer.js
+++ inquirer.js
@@ -1,48 +1,85 @@
-var inquirer = require('inquirer');
+#!/usr/bin/env node
 
-// TODO 格式 大小 是否遍历 是否覆盖
+const inquirer = require("inquirer");
+const { startCompress } = require("./compress");
+const { mb2b } = require("./utils");
+
+// 获取命令执行路径
+const cwd = process.cwd();
+
+const DEFAULT_SIZE = 5;
 
 inquirer
   .prompt([
     {
-      type: 'checkbox',
-      message: 'Select image format',
-      name: 'format',
+      type: "checkbox",
+      message: "Select image format",
+      name: "ext",
+      default: [".jpg", ".jpeg", ".png"],
       choices: [
         {
-          name: 'jpg',
+          name: ".jpg",
         },
         {
-          name: 'png',
+          name: ".jpeg",
+        },
+        {
+          name: ".png",
+        },
+        {
+          name: ".bmp",
+        },
+        {
+          name: ".webp",
         },
       ],
       validate(answer) {
         if (answer.length < 1) {
-          return 'You must choose at least one image format.';
+          return "You must choose at least one image format.";
         }
 
         return true;
       },
     },
     {
-      type: 'input',
-      name: 'maxSize',
-      message: "Input image max size.",
+      type: "input",
+      name: "maxSize",
+      message: "Input image max size (MB).",
       default() {
-        return 5200000;
+        return DEFAULT_SIZE;
       },
-      validate(value) {
-        const pass = value.match(
-          /^\d{1,}$/
-        );
+      validate(value = DEFAULT_SIZE) {
+        const pass =
+          value.toString().match(/^[1-9]{1,}\d*$/) && value <= DEFAULT_SIZE;
         if (pass) {
           return true;
         }
-  
-        return 'Please enter a valid phone number';
+
+        return "Please enter a valid max size";
       },
+    },
+
+    {
+      type: "confirm",
+      name: "isOverwrite",
+      message: "Whether to overwrite the source file?",
+      default: false,
+    },
+    {
+      type: "confirm",
+      name: "isRecursion",
+      message: "Whether recursion?",
+      default: false,
     },
   ])
   .then((answers) => {
-    console.log(JSON.stringify(answers, null, '  '));
+    const { ext, maxSize, isOverwrite, isRecursion } = answers;
+
+    // TODO 不想使用透传，所以将参数设置为全局变量
+    global.ext = ext;
+    global.maxSize = mb2b(maxSize);
+    global.isOverwrite = isOverwrite;
+    global.isRecursion = isRecursion;
+
+    startCompress(cwd);
   });
